307411a2b54103cd57b11fc29d90c543357dc0c9
SauceX <mahmoudzak4@gmail.com>
2023-04-12 05:39:18 +0300

    fix: flag change on locale change + minor margin fix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,14 @@
 import { useTranslation } from "next-i18next";
 import React from "react";
-import LocaleSwitch from './locale/LocaleSwitch';
+import LocaleSwitch, { USFlag, SAFlag } from './locale/LocaleSwitch';
+import { useRouter } from "next/router";
 
 type Props = {
   // custom props here...
 };
 
 const Sidebar = (props: Props) => {
+  const { locale } = useRouter();
   const { t } = useTranslation("common");
 
   return (
@@ -357,44 +359,13 @@ const Sidebar = (props: Props) => {
           data-dropdown-toggle="language-dropdown"
           className="inline-flex cursor-pointer justify-center rounded p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
         >
-          <svg
-            aria-hidden="true"
-            className="mt-0.5 h-5 w-5 rounded-full"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            viewBox="0 0 3900 3900"
-          >
-            <path fill="#b22234" d="M0 0h7410v3900H0z" />
-            <path
-              d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0"
-              stroke="#fff"
-              stroke-width="300"
-            />
-            <path fill="#3c3b6e" d="M0 0h2964v2100H0z" />
-            <g fill="#fff">
-              <g id="d">
-                <g id="c">
-                  <g id="e">
-                    <g id="b">
-                      <path
-                        id="a"
-                        d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"
-                      />
-                      <use xlinkHref="#a" y="420" />
-                      <use xlinkHref="#a" y="840" />
-                      <use xlinkHref="#a" y="1260" />
-                    </g>
-                    <use xlinkHref="#a" y="1680" />
-                  </g>
-                  <use xlinkHref="#b" x="247" y="210" />
-                </g>
-                <use xlinkHref="#c" x="494" />
-              </g>
-              <use xlinkHref="#d" x="988" />
-              <use xlinkHref="#c" x="1976" />
-              <use xlinkHref="#e" x="2470" />
-            </g>
-          </svg>
+          <div className="m-auto">
+            {locale === "en" ?
+              <USFlag />
+              :
+              <SAFlag />
+            }
+          </div>
         </button>
         {/* <!-- Dropdown --> */}
         <LocaleSwitch />
